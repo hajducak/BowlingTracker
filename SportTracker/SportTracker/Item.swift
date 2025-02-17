fcec03623aa79ef0f31569c81c644abd434b0014
Marek Hajdučák <hajducakmarek@gmail.com>
2025-02-17 16:00:22 +0100

    Initial project setup

---
+++ SportTracker/SportTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SportTracker
+//
+//  Created by MacBook Pro on 17/02/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}

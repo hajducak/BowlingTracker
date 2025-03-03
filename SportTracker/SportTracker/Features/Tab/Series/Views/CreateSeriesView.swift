fc42d63c1db61dc6376f1fd40e83633cbc5a6212
Marek Hajdučák <hajducakmarek@gmail.com>
2025-03-03 16:28:02 +0100

    add sticky buttons view on create new series + screenshots update

--- SportTracker/SportTracker/Features/Tab/Series/Views/CreateSeriesView.swift
+++ SportTracker/SportTracker/Features/Tab/Series/Views/CreateSeriesView.swift
@@ -9,65 +9,79 @@ struct CreateSeriesView: View {
     var onClose: () -> Void
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Create new series")
-                    .font(.largeTitle).bold()
-                TextField("Enter series name", text: $seriesName)
-                    .textFieldStyle(labeled: "Series name")
-                TextField("Enter series description", text: $seriesDescription)
-                    .textFieldStyle(labeled: "Series description")
-                Picker("Select type", selection: $selectedType) {
-                    Text("League").tag(SeriesType.league)
-                    Text("Tournament").tag(SeriesType.tournament)
-                    Text("Training").tag(SeriesType.training)
-                    Text("Other").tag(SeriesType.other)
+        ZStack(alignment: .bottom) {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Create new series")
+                        .font(.largeTitle).bold()
+                    TextField("Enter series name", text: $seriesName)
+                        .textFieldStyle(labeled: "Series name")
+                    TextField("Enter series description", text: $seriesDescription)
+                        .textFieldStyle(labeled: "Series description")
+                    Picker("Select type", selection: $selectedType) {
+                        Text("League").tag(SeriesType.league)
+                        Text("Tournament").tag(SeriesType.tournament)
+                        Text("Training").tag(SeriesType.training)
+                        Text("Other").tag(SeriesType.other)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .font(.system(size: 16, weight: .medium))
+                    .labled(label: "Type of series")
+                    DatePicker("", selection: $selectedDate, displayedComponents: .date)
+                        .datePickerStyle(GraphicalDatePickerStyle())
+                        .tint(.orange)
+                        .background(UIColor.systemGray6.color.cornerRadius(8))
+                        .labled(label: "Select date")
+                    Spacer()
                 }
-                .pickerStyle(SegmentedPickerStyle())
-                .font(.system(size: 16, weight: .medium))
-                .labled(label: "Type of series")
-                DatePicker("", selection: $selectedDate, displayedComponents: .date)
-                    .datePickerStyle(GraphicalDatePickerStyle())
-                    .tint(.orange)
-                    .background(UIColor.systemGray6.color.cornerRadius(8))
-                    .labled(label: "Select date")
-                Spacer()
+                .padding(.horizontal, Padding.defaultPadding)
+                .padding(.bottom, 100)
             }
-            .padding(.horizontal, Padding.defaultPadding)
-            HStack {
+            buttons
+                .padding(.vertical, Padding.defaultPadding)
+                .padding(.bottom, Padding.defaultPadding)
+                .background {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.white)
+                        .stroke(UIColor.systemGray4.color, lineWidth: 1)
+                }
+        }.edgesIgnoringSafeArea(.bottom)
+    }
+
+    private var buttons: some View {
+        HStack {
+            Button {
+                onClose()
+            } label: {
+                Text("Close")
+                    .font(.system(size: 16, weight: .bold))
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .foregroundColor(.orange)
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.orange, lineWidth: 1)
+            )
+            .frame(maxWidth: .infinity)
+            Spacer()
+            onSave.map { _ in
                 Button {
-                    onClose()
+                    onSave?()
                 } label: {
-                    Text("Close")
+                    Text("Save")
                         .font(.system(size: 16, weight: .bold))
                         .padding()
                         .frame(maxWidth: .infinity)
-                        .background(Color.white)
-                        .foregroundColor(.orange)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
                 }
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.orange, lineWidth: 1)
-                )
                 .frame(maxWidth: .infinity)
-                Spacer()
-                onSave.map { _ in
-                    Button {
-                        onSave?()
-                    } label: {
-                        Text("Save")
-                            .font(.system(size: 16, weight: .bold))
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(Color.orange)
-                            .foregroundColor(.white)
-                            .cornerRadius(12)
-                    }
-                    .frame(maxWidth: .infinity)
-                }
             }
-            .padding(.horizontal, Padding.defaultPadding)
         }
+        .padding(.horizontal, Padding.defaultPadding)
     }
 }
 
